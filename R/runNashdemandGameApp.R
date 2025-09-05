03b486a134fe1b94fecb66ba93aaa4dc064739f0
bangecon <bangjamest@sau.edu>
2025-09-05 09:18:37 -0500

    Add runNashdemandGameApp()

---
+++ R/runNashdemandGameApp.R
@@ -0,0 +1,8 @@
+#' @export
+runUltimatumGameApp <- function() {
+  appDir <- system.file("shiny-examples", "nashdemandGameApp", package = "econGame365")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `econGame`.", call. = FALSE)
+  }
+  shiny::runApp(appDir, display.mode = "normal")
+}
